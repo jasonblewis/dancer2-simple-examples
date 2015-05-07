8ff020858a9cf0e1dfa72b801e29a38874a79bb1
Jason Lewis <jason@dickson.st>
2015-05-07 10:35:51 +1000

    begining of HTML::FormHandler example

---
+++ dbicapp/demo.pl
@@ -0,0 +1,38 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use MyApp::Schema;
+
+use Dancer2;
+
+set 'session'      => 'Simple';
+set 'template'     => 'template_toolkit';
+set 'logger'       => 'console';
+set 'log'          => 'debug';
+set 'show_errors'  => 1;
+set 'startup_info' => 1;
+set 'warnings'     => 1;
+set 'layout'       => 'main';
+
+
+use Path::Class 'file';
+
+get '/' => sub {
+    my $db_fn = file($INC{'MyApp/Schema.pm'})->dir->parent->file('db/example.db');
+    # for other DSNs, e.g. MySql, see the perldoc for the relevant dbd
+    # driver, e.g perldoc L<DBD::mysql>.
+    my $schema = MyApp::Schema->connect("dbi:SQLite:$db_fn");
+    my @all_artists = $schema->resultset('Artist')->all;
+    template 'artists', {
+        artists => @all_artists,
+    };
+    
+};
+
+
+start;
+
+
+
